e387a02dcf98beae0674de8d3a0de5606710af42
Thiago <gmoliveira69@gmail.com>
2022-05-07 21:43:57 -0300

    tests

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import { routes } from './routes'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 app.use(express.json())
-
 app.use(routes)
 
 app.listen(3333, () => {
